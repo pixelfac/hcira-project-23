ce8a9058ddf289bd4183e64a6690008983c45a8d
pixelfac <n.harris1@ufl.edu>
2023-01-28 19:02:05 -0500

    remove unused import

--- draw.py
+++ draw.py
@@ -1,5 +1,10 @@
-import tkinter as tk
 from tkinter import ttk
+
+def draw_line(event):
+    if canvas.old_coords: # if canvas object has property 'old_coords'
+        x1, y1 = canvas.old_coords
+        canvas.create_line(event.x, event.y, x1, y1)
+    canvas.old_coords = event.x, event.y
 
 win = tk.Tk()           # init window
 win.geometry("400x400") # set window dimensions
@@ -8,11 +13,7 @@ canvas = tk.Canvas(win, width=400, height=400)
 canvas.pack() # pack adds canvas to parent (window), and also can adjust scaling/sizing relative to parent
 canvas.old_coords = None
 
-def draw_line(event):
-    if canvas.old_coords: # if canvas object has property 'old_coords'
-        x1, y1 = canvas.old_coords
-        canvas.create_line(event.x, event.y, x1, y1)
-    canvas.old_coords = event.x, event.y
+
 
 win.bind('<ButtonPress-1>', draw_line)
 win.mainloop()
