1c4a4d2df0d8203e5e9f092ba2326c6a67f5adc6
pixel <43653484+pixelfac@users.noreply.github.com>
2023-02-21 16:17:13 -0600

    add example of data structure

--- src/load_data.py
+++ src/load_data.py
@@ -6,4 +6,20 @@ data - list of users
     user - list of gestures
         gestures - list of examples (each xml file)
 
+
+data
+    user 1
+        arrow
+            arrow1
+            arrow2
+        check
+            check1
+            check2
+    user 2
+        arrow
+            arrow1
+            arrow2
+        check
+            check1
+            check2          
 """
