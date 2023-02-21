217fbe6719b589ce9ce804cf891332f58ca6a8fb
pixel <43653484+pixelfac@users.noreply.github.com>
2023-02-21 15:45:39 -0600

    add description for data object

--- src/load_data.py
+++ src/load_data.py
@@ -1,2 +1,9 @@
 # xml files are read and loaded into variable called 'data' that matches the form expected
 # in test.py (see comments at top of file)
+
+"""
+data - list of users
+    user - list of gestures
+        gestures - list of examples (each xml file)
+
+"""
