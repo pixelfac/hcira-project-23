8a9ee45e763e5e3060d16953c49fa645d777a047
pixel <43653484+pixelfac@users.noreply.github.com>
2023-02-21 15:30:20 -0600

    add blank load_data.py

---
+++ src/load_data.py
@@ -0,0 +1,2 @@
+# xml files are read and loaded into variable called 'data' that matches the form expected
+# in test.py (see comments at top of file)
--- src/test.py
+++ src/test.py
@@ -1,26 +1,14 @@
 import random
 from preprocess_dollar_one import recognize
+# from load_data import data
 
-'''
-for each user U = 1 to 10 
-  for each example E = 1 to 9 
-    for i = 1 to 100 
-      for each gesture type G 
-        choose E templates from U,G set 
-        choose 1 candidate from U,G set 
-      for each candidate T from 1 to G 
-        recognize T w/ E,G chosen templates 
-        if reco correct 
-          reco score for each U,G += 1 
-    reco score for each U,G /= 100 
-report final average per-user accuracy
-'''
 
 '''
 data is list of list of lists, accessing first by user, then by gesture,
 then you have a list of all examples of that gesture by that user, where example is a Unistroke object
 
 recognize_score is list of list of numbers, tracking the recognize score per user, per gesture
+
 recognize_acc is list of list of numbers, tracking %correct per user, per gesture
 '''
 # number ot times to tests each group
