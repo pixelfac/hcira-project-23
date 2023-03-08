6b38592529281dfe5ed402d81893da0f6fa963ce
pixel <43653484+pixelfac@users.noreply.github.com>
2023-03-08 10:44:34 -0600

    fix data naming convention

--- src/dataset/fix_xml.py
+++ src/dataset/fix_xml.py
@@ -2,7 +2,7 @@ import os
 
 users = os.listdir('./')
 # remove this file from list
-users = users[1:4]
+users = users[1:]
 print(users)
 
 
@@ -10,13 +10,22 @@ for user in users:
 
     samples = os.listdir('./' + user + '/')
     for sample in samples:
-        print('./' + user + '/' + sample)
+        # print('./' + user + '/' + sample)
         with open('./' + user + '/' + sample, 'r') as f:
             text = f.readlines()
 
-        for i in range(2, len(text)-1):
-            text[i] = text[i][:-3] + f'T="{i}" />\n'
+        line = text[1]
+        first_quote = line.find('"')
+        second_quote = line.find('"', first_quote+1)
+        sample_name = line[first_quote+1:second_quote]
 
+        # add 0 to single digit numbers
+        if not sample_name.endswith('10'):
+            sample_name = sample_name[:-1] + '0' + sample_name[-1:]
+            
+            line = line[:first_quote+1] + sample_name + line[second_quote:]
+
+        text[1] = line
         # clear and rewrite
         with open('./' + user + '/' + sample, 'w') as f:
             f.writelines(text)
