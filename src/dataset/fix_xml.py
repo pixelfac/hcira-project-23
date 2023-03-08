3a968c87e866b2a7e4d1534c81a1881c4ba91648
pixel <43653484+pixelfac@users.noreply.github.com>
2023-03-08 10:24:04 -0600

    add code to fix missing time info

--- src/dataset/fix_xml.py
+++ src/dataset/fix_xml.py
@@ -2,20 +2,21 @@ import os
 
 users = os.listdir('./')
 # remove this file from list
-users = users[4:]
+users = users[1:4]
 print(users)
 
 
 for user in users:
+
     samples = os.listdir('./' + user + '/')
     for sample in samples:
         print('./' + user + '/' + sample)
         with open('./' + user + '/' + sample, 'r') as f:
             text = f.readlines()
 
-        text[1] = text[1][:-2] + '">\n'
+        for i in range(2, len(text)-1):
+            text[i] = text[i][:-3] + f'T="{i}" />\n'
 
         # clear and rewrite
         with open('./' + user + '/' + sample, 'w') as f:
             f.writelines(text)
-            
